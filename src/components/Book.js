c8c643c2a08e53f71f8f18e442707eb424e98653
tGodson <tendongze95@gmail.com>
2020-12-14 00:05:51 +0100

    fix all TSE errors

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ book }) => (
+const Book = ({ book, handleRemoveBook }) => (
   <tr>
     <td>{book.id}</td>
     <td>{book.title}</td>
     <td>{book.category}</td>
+    <td><button type="button" onClick={() => handleRemoveBook(book)}>Remove</button></td>
   </tr>
 );
 
@@ -15,6 +16,7 @@ Book.propTypes = {
     title: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
   }).isRequired,
+  handleRemoveBook: PropTypes.func.isRequired,
 };
 
 export default Book;
